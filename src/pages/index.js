e5ebe4ff17e22ad9cccf18ee3fc272ff0990af2e
Ritesh Kumar <riteshkumar007software@gmail.com>
2019-08-18 13:26:36 +0530

    New Project

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Home page</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    
+    </div>
+
+  </Layout>
+)
+
+export default IndexPage
