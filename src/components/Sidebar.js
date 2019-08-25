d68ea1b13d8551a0ef4b6a41d68663c9ea4902d5
Ritesh Kumar <31771212+Rksensational@users.noreply.github.com>
2019-08-25 15:41:00 +0530

    Added Contact Form in Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -97,6 +97,35 @@ const Sidebar = ({ author, authorFluid }) => (
         </Form>
       </CardBody>
     </Card>
+
+<Card>
+      <CardBody>
+        <CardTitle className="text-center text-uppercase mb-3">
+          Contact Form
+        </CardTitle>
+        <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+      </CardBody>
+    </Card>
+
     <Card>
       <CardBody>
         <CardTitle className="text-center text-uppercase">
