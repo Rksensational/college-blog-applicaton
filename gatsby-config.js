a9142f5463ec20d93da0a7f45e1df71c79193fc1
Ritesh Kumar <riteshkumar007software@gmail.com>
2019-10-03 20:36:11 +0530

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `College Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Blog Website`,
+    author: `@RiteshKumar`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
